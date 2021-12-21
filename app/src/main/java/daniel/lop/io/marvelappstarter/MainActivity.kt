5bd73d1b6394780a10842d1f5ac00d22c1e1a068
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-12-21 18:23:13 -0300

    newScreenNavGraph

--- app/src/main/java/daniel/lop/io/marvelappstarter/MainActivity.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/MainActivity.kt
@@ -2,15 +2,33 @@ package daniel.lop.io.marvelappstarter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import daniel.lop.io.marvelappstarter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navHostFragment: NavHostFragment
     private lateinit var binding: ActivityMainBinding
+    //private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews(binding)
+
+    }
+
+    private fun initViews(binding: ActivityMainBinding) {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener {  } //evita fazer o recarregamento da pag, caso clicarmos no mesmo botao denovo
+        }
     }
 }
