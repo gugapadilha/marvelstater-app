6ca139ecd420b52d2bc0cf390f951d5c896eab92
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-12-22 11:12:29 -0300

    creatingDI

--- app/src/main/java/daniel/lop/io/marvelappstarter/di/Module.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/di/Module.kt
@@ -1,4 +1,40 @@
 package daniel.lop.io.marvelappstarter.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import daniel.lop.io.marvelappstarter.data.remote.ServiceApi
+import daniel.lop.io.marvelappstarter.util.Constants.BASE_URL
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object Module {
+
+    @Singleton //permite que a gente atualize a busca dos dados, seja pela api, ou banco de dados (singleton mantem uma instancia unica do okhttp durante toda a execução
+    @Provides //ele provê essa instancia que a gente quer
+    fun provideOkHttpClient() : OkHttpClient{
+        return  OkHttpClient()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient): Retrofit{ //essa funcao tem uma depencia do okhttp cliente, pra ela funcionar, tem que passar primeiro pela okhttpclient
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideServiceApi(retrofit: Retrofit): ServiceApi{
+        return retrofit.create(ServiceApi::class.java)
+    }
+
 }
