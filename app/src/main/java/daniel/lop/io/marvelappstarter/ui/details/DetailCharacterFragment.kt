860a784cfeaa7e52d9413d881f9528b751dadbe3
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-12-25 11:00:10 -0300

    showImage&DescriptionCharacters

--- app/src/main/java/daniel/lop/io/marvelappstarter/ui/details/DetailCharacterFragment.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/ui/details/DetailCharacterFragment.kt
@@ -1,37 +1,147 @@
 package daniel.lop.io.marvelappstarter.ui.details
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
+import daniel.lop.io.marvelappstarter.R
+import daniel.lop.io.marvelappstarter.data.model.character.CharacterModel
 import daniel.lop.io.marvelappstarter.databinding.FragmentDetailsCharacterBinding
 import daniel.lop.io.marvelappstarter.databinding.FragmentListCharacterBinding
+import daniel.lop.io.marvelappstarter.ui.adapters.ComicAdapter
 import daniel.lop.io.marvelappstarter.ui.base.BaseFragment
+import daniel.lop.io.marvelappstarter.ui.state.ResourceState
+import daniel.lop.io.marvelappstarter.util.hide
+import daniel.lop.io.marvelappstarter.util.limitDescription
+import daniel.lop.io.marvelappstarter.util.show
+import daniel.lop.io.marvelappstarter.util.toast
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class DetailCharacterFragment :
     BaseFragment<FragmentDetailsCharacterBinding, DetailsCharacterViewModel>() {
-    //override val viewModel: DetailsCharacterViewModel by viewModels()
-    val args: DetailCharacterFragmentArgs by navArgs()
+    override val viewModel: DetailsCharacterViewModel by viewModels()
+
+    private val args: DetailCharacterFragmentArgs by navArgs()
+    private val comicAdapter by lazy {ComicAdapter()}
+    private lateinit var characterModel: CharacterModel
 
 
-    override fun onResume() {
-        super.onResume()
-        binding.tvNameCharacterDetails.text = args.character.name
-        binding.tvDescriptionCharacterDetails.text = args.character.description
-            Glide.with(this)
-                .load(args.character.thumbnailModel.path + "." + args.character.thumbnailModel.extension)
-                .into(binding.imgCharacterDetails)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        characterModel = args.character
+        viewModel.fetch(characterModel.id)
+        setupRecycleView()
+        onLoadedCharacter(characterModel)
+        collectObserver()
+        binding.tvDescriptionCharacterDetails.setOnClickListener {
+            onShowDialog(characterModel)
+        }
     }
 
-    override val viewModel: DetailsCharacterViewModel
-        get() = TODO("Not yet implemented")
+    private fun onShowDialog(characterModel: CharacterModel) {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(characterModel.name)
+            .setMessage(characterModel.description)
+            .setNegativeButton(getString(R.string.close_dialog)){dialog, _ ->
+                dialog.dismiss() //para fechar detalhes
+            }
+            .show() //para abrir detalhes
+    }
+
+    private fun collectObserver() = lifecycleScope.launch {
+        viewModel.details.collect { result ->
+            when(result){
+                is ResourceState.Success -> {
+                    binding.progressBarDetail.hide()
+                    result.data?.let { values ->
+                        if (values.data.result.count() > 0){
+                            comicAdapter.characters = values.data.result.toList()
+                        }else {
+                            toast(getString(R.string.empty_list_comics))
+                        }
+                    }
+                }
+
+                is ResourceState.Error -> {
+                    binding.progressBarDetail.hide()
+                    result.message?.let { message ->
+                        Timber.tag("DetailsCharacter").e("Error -> $message")
+                        toast(getString(R.string.an_error_occurred))
+                    }
+                }
+
+                is ResourceState.Loading -> {
+                    binding.progressBarDetail.show()
+                }
+                else -> {}
+            }
+        }
+
+    }
+
+    private fun onLoadedCharacter(characterModel: CharacterModel) = with(binding) {
+        tvNameCharacterDetails.text = characterModel.name
+        if (characterModel.description.isEmpty()){
+            tvDescriptionCharacterDetails.text = requireContext().getString(R.string.text_description_empty)
+        }else {
+            tvDescriptionCharacterDetails.text = characterModel.description.limitDescription(100)
+        }
+        Glide.with(requireContext())
+            .load(characterModel.thumbnailModel.path + "." + args.character.thumbnailModel.extension)
+            .into(imgCharacterDetails)
+    }
+
+    private fun setupRecycleView() = with(binding) {
+        rvComics.apply {
+            adapter = comicAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_details, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.favorite -> {
+                //viewModel.insert(characterModel)
+                toast(getString(R.string.saved_successfully))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true) //mostrar a barra de menu para poder favoritar
+        super.onCreate(savedInstanceState)
+    }
 
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentDetailsCharacterBinding = FragmentDetailsCharacterBinding
         .inflate(inflater, container, false)
+
+
+//    override fun onResume() { //outra forma de implementar a imagem.
+//        super.onResume()
+//        binding.tvNameCharacterDetails.text = args.character.name
+//        binding.tvDescriptionCharacterDetails.text = args.character.description
+//            Glide.with(this)
+//                .load(args.character.thumbnailModel.path + "." + args.character.thumbnailModel.extension)
+//                .into(binding.imgCharacterDetails)
+//    }
+
+
 
 }
--- app/src/main/java/daniel/lop/io/marvelappstarter/ui/details/DetailsCharacterViewModel.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/ui/details/DetailsCharacterViewModel.kt
@@ -1,6 +1,49 @@
 package daniel.lop.io.marvelappstarter.ui.details
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import daniel.lop.io.marvelappstarter.data.model.comic.ComicModelResponse
+import daniel.lop.io.marvelappstarter.repository.MarvelRepository
+import daniel.lop.io.marvelappstarter.ui.state.ResourceState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import java.io.IOException
+import javax.inject.Inject
 
-class DetailsCharacterViewModel: ViewModel() {
+@HiltViewModel
+class DetailsCharacterViewModel @Inject constructor(
+    private val repository: MarvelRepository
+): ViewModel() {
+
+    private val _details = MutableStateFlow<ResourceState<ComicModelResponse>>(ResourceState.Loading())
+    val details: StateFlow<ResourceState<ComicModelResponse>> = _details
+
+    fun fetch(characterId: Int) = viewModelScope.launch{
+        safeFetch(characterId)
+    }
+
+    private suspend fun safeFetch(characterId: Int) {
+        _details.value = ResourceState.Loading()
+        try {
+            val response = repository.getComics(characterId)
+            _details.value = handleResponse(response)
+        }catch (t: Throwable){
+            when(t){
+                is IOException -> _details.value = ResourceState.Error("Erro de rede ou conexão com internet")
+                else -> _details.value = ResourceState.Error("Erro na conversão")
+            }
+        }
+    }
+
+    private fun handleResponse(response: Response<ComicModelResponse>): ResourceState<ComicModelResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { values ->
+                return ResourceState.Success(values)
+            }
+        }
+        return ResourceState.Error(response.message())
+    }
 }
